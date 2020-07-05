7b9139c1ba022aa4357383a62ffcbb2f616a57d1
GoG_Paulie <ppolih21@gmail.com>
2020-07-05 19:12:20 -0400

    Updates

---
+++ precache-manifest.da3ef127b8616107c5bbedcf97cd568b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617c4261df1ccd036403dc2e5e23e31a",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "c4510c489851c67618e5",
+    "url": "/TodoApp/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "e3814683527e01043694",
+    "url": "/TodoApp/static/js/2.b1ffa20e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoApp/static/js/2.b1ffa20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4510c489851c67618e5",
+    "url": "/TodoApp/static/js/main.f09adc51.chunk.js"
+  },
+  {
+    "revision": "c662825f8d338341bcb9",
+    "url": "/TodoApp/static/js/runtime-main.6e726f31.js"
+  }
+]);
